a67c91de7b956aa955a99b6eb6b47f0089a2f858
Tanya Mandloi <136952090+TanyaMandloi05@users.noreply.github.com>
2025-11-15 16:53:43 +0530

    Update ClassRoom.java

--- 34.GreedyAlgorithm/ClassRoom.java
+++ 34.GreedyAlgorithm/ClassRoom.java
@@ -72,41 +72,41 @@
 // }
 // }
 
-// // Que Fractional knapsack...
-// import java.util.*;
-// public class ClassRoom {
-//     public static void main(String args[]) {
-//         int val[] = {60, 100, 120};
-//         int weight[] = {10, 20, 30};
-//         int w = 50;
+// Que Fractional knapsack...
+import java.util.*;
+public class ClassRoom {
+    public static void main(String args[]) {
+        int val[] = {60, 100, 120};
+        int weight[] = {10, 20, 30};
+        int w = 50;
 
-//         double ratio[][] = new double[val.length][2];
-//         //0th col -> idx, 1st col -> ratio
-//         for(int i=0; i<val.length; i++) {
-//             ratio[i][0] = i;
-//             ratio[i][1] = val[i]/(double)weight[i];
-//         }
+        double ratio[][] = new double[val.length][2];
+        //0th col -> idx, 1st col -> ratio
+        for(int i=0; i<val.length; i++) {
+            ratio[i][0] = i;
+            ratio[i][1] = val[i]/(double)weight[i];
+        }
 
-//         Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+        Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
 
-//         int capacity = w;
-//         int finalVal = 0;
+        int capacity = w;
+        int finalVal = 0;
 
-//         for(int i=ratio.length-1; i>=0; i--) {
-//             int idx = (int)ratio[i][0];
-//             if(capacity > weight[idx]) {
-//                 finalVal += val[idx];
-//                 capacity -= weight[idx];
-//             } else {
-//                 finalVal += ratio[i][1] * capacity;
-//                 capacity = 0;
-//                 break;
-//             }
-//         }
+        for(int i=ratio.length-1; i>=0; i--) {
+            int idx = (int)ratio[i][0];
+            if(capacity > weight[idx]) {
+                finalVal += val[idx];
+                capacity -= weight[idx];
+            } else {
+                finalVal += ratio[i][1] * capacity;
+                capacity = 0;
+                break;
+            }
+        }
 
-//         System.out.println("final value = " + finalVal);
-//     }
-// }
+        System.out.println("final value = " + finalVal);
+    }
+}
 
 
 // //Minimum sum absolute diffrence pair..
