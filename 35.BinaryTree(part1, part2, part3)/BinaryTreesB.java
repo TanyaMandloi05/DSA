ea493ebb23e20eaa84f4a8779a489bcd3997a65d
Tanya Mandloi <136952090+TanyaMandloi05@users.noreply.github.com>
2026-01-03 11:38:29 +0530

    Uncomment and implement lowest common ancestor logic

--- 35.BinaryTree(part1, part2, part3)/BinaryTreesB.java
+++ 35.BinaryTree(part1, part2, part3)/BinaryTreesB.java
@@ -558,82 +558,82 @@ public class BinaryTreesB {
 // }
 
 
-// //Lowest common ancestor
-// import java.util.*;
-// public class BinaryTreesB {
+//Lowest common ancestor
+import java.util.*;
+public class BinaryTreesB {
 
-//      static class Node {
-//         int data; 
-//         Node left, right;
+     static class Node {
+        int data; 
+        Node left, right;
 
-//         public Node(int data) {
-//             this.data = data;
-//             this.left = null;
-//             this.right = null;
-//         }
-//     }
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
 
-//     public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
         
-//         if(root == null) {
-//             return false;
-//         }
+        if(root == null) {
+            return false;
+        }
 
-//         path.add(root);
+        path.add(root);
 
-//         if(root.data == n) {
-//             return true;
-//         }
+        if(root.data == n) {
+            return true;
+        }
 
 
-//         boolean leftPath = getPath(root.left, n, path);
-//         boolean rightPath = getPath(root.right, n, path);
+        boolean leftPath = getPath(root.left, n, path);
+        boolean rightPath = getPath(root.right, n, path);
 
-//         if(leftPath || rightPath) {
-//             return true;
-//         }
+        if(leftPath || rightPath) {
+            return true;
+        }
 
-//         path.remove(path.size() - 1);
-//         return false;
-//     }
+        path.remove(path.size() - 1);
+        return false;
+    }
 
-//     public static Node Lca(Node root, int n1, int n2) {
-//     ArrayList<Node> path1 = new ArrayList<>();
-//     ArrayList<Node> path2 = new ArrayList<>();
+    public static Node Lca(Node root, int n1, int n2) {
+    ArrayList<Node> path1 = new ArrayList<>();
+    ArrayList<Node> path2 = new ArrayList<>();
 
-//     getPath(root, n1, path1);
-//     getPath(root, n2, path2);
+    getPath(root, n1, path1);
+    getPath(root, n2, path2);
 
-//     int i = 0;
-//     for(; i < path1.size() && i < path2.size(); i++) {
-//         if(path1.get(i) != path2.get(i)) {
-//             break;
-//         }
-//      }
-//      Node lca = path1.get(i-1);
-//       return lca;
-//     }
+    int i = 0;
+    for(; i < path1.size() && i < path2.size(); i++) {
+        if(path1.get(i) != path2.get(i)) {
+            break;
+        }
+     }
+     Node lca = path1.get(i-1);
+      return lca;
+    }
 
-//     public static void main(String args[]) {
-//  //         1
-// //        /  \
-// //       2    3
-// //     /  \  /  \
-// //    4    5 6   7
+    public static void main(String args[]) {
+ //         1
+//        /  \
+//       2    3
+//     /  \  /  \
+//    4    5 6   7
 
-//    Node root = new Node(1);
-//    root.left = new Node(2);
-//    root.right = new Node(3);
-//    root.left.left = new Node(4);
-//    root.left.right = new Node(5);
-//    root.right.left = new Node(6);
-//    root.right.right = new Node(7);
+   Node root = new Node(1);
+   root.left = new Node(2);
+   root.right = new Node(3);
+   root.left.left = new Node(4);
+   root.left.right = new Node(5);
+   root.right.left = new Node(6);
+   root.right.right = new Node(7);
 
-//    int n1 = 4;
-//    int n2 = 7;
-//    System.out.println(Lca(root, n1, n2).data);
-//     }
-// }
+   int n1 = 4;
+   int n2 = 7;
+   System.out.println(Lca(root, n1, n2).data);
+    }
+}
 
 
 // //Lowest common ancestor(Approch 2)
