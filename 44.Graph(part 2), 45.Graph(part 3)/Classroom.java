83bee4ca241e2a86f59128e1900695c7eeedb94c
Tanya Mandloi <136952090+TanyaMandloi05@users.noreply.github.com>
2026-02-12 21:44:23 +0530

    Uncomment and implement Classroom graph methods

--- 44.Graph(part 2), 45.Graph(part 3)/Classroom.java
+++ 44.Graph(part 2), 45.Graph(part 3)/Classroom.java
@@ -339,84 +339,84 @@
 //     }
 // }
 
-// //IsBiapartite..
-// import java.util.*;
-// public class Classroom {
+//IsBiapartite..
+import java.util.*;
+public class Classroom {
 
-//     static class Edge {
-//         int src;
-//         int dest;
+    static class Edge {
+        int src;
+        int dest;
 
-//         public Edge(int src, int dest) {
-//             this.src = src;
-//             this.dest = dest;
-//         }
-//     }
+        public Edge(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
+        }
+    }
 
-//     public static void createGraph(ArrayList<Edge> graph[], int V) {
-//         for(int i=0; i<V; i++) {
-//             graph[i] = new ArrayList<>();
-//         }
+    public static void createGraph(ArrayList<Edge> graph[], int V) {
+        for(int i=0; i<V; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-//         graph[0].add(new Edge(0, 1));
-//         graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
 
-//         graph[1].add(new Edge(1, 3));
-//         graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+        graph[1].add(new Edge(1, 0));
 
-//         graph[2].add(new Edge(2, 0));
-//         graph[2].add(new Edge(2, 4));
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
 
-//         graph[3].add(new Edge(3, 1));
-//         graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
 
-//         graph[4].add(new Edge(4, 2));
-//         graph[4].add(new Edge(4, 3));
-//     }
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+    }
     
-//     public static boolean isBipartite(ArrayList<Edge> graph[]) {
+    public static boolean isBipartite(ArrayList<Edge> graph[]) {
         
-//         int color[] = new int[graph.length];
-//         for(int i=0; i<color.length; i++) {
-//             color[i] = -1; // no color;
-//         }
+        int color[] = new int[graph.length];
+        for(int i=0; i<color.length; i++) {
+            color[i] = -1; // no color;
+        }
 
-//         Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
 
-//          for(int i=0; i<graph.length; i++) {
-//             if(color[i] == -1) {
-//                  q.add(i);
-//                  color[i] = 0;
+         for(int i=0; i<graph.length; i++) {
+            if(color[i] == -1) {
+                 q.add(i);
+                 color[i] = 0;
 
-//                  while(!q.isEmpty()) {
-//                     int curr = q.remove();
+                 while(!q.isEmpty()) {
+                    int curr = q.remove();
 
-//                     for(int j=0; j<graph[curr].size(); j++) {
-//                         Edge e = graph[curr].get(j);
+                    for(int j=0; j<graph[curr].size(); j++) {
+                        Edge e = graph[curr].get(j);
 
-//                         if(color[e.dest] == -1) {
-//                             int nextColor = color[curr] == 0 ? 1 : 0;
-//                             color[e.dest] = nextColor;
-//                             q.add(e.dest);
+                        if(color[e.dest] == -1) {
+                            int nextColor = color[curr] == 0 ? 1 : 0;
+                            color[e.dest] = nextColor;
+                            q.add(e.dest);
 
-//                         } else if(color[e.dest] == color[curr]) {
-//                             return false;
-//                         }
-//                     }
-//                  }
-//             }
-//         }
+                        } else if(color[e.dest] == color[curr]) {
+                            return false;
+                        }
+                    }
+                 }
+            }
+        }
 
-//         return true;
-//     }
+        return true;
+    }
 
-//     public static void main(String args[]) {
-//         int V = 5;
-//         ArrayList<Edge>[] graph = new ArrayList[V];
-//         createGraph(graph, V);
-//         System.out.print(isBipartite(graph));
-//     }
-// }
+    public static void main(String args[]) {
+        int V = 5;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph, V);
+        System.out.print(isBipartite(graph));
+    }
+}
 
 
 // //Topological sorting using dfs..
